e58decab400bada1d84fb4a7966931981b785e27
gregstoll <greg@gregstoll.com>
2022-05-23 20:28:43 -0500

    simple usage of pathfinding

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use pathfinding::prelude::bfs;
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Pos(i32, i32);
+
+impl Pos {
+  fn successors(&self) -> Vec<Pos> {
+    let &Pos(x, y) = self;
+    vec![Pos(x+1,y+2), Pos(x+1,y-2), Pos(x-1,y+2), Pos(x-1,y-2),
+         Pos(x+2,y+1), Pos(x+2,y-1), Pos(x-2,y+1), Pos(x-2,y-1)]
+  }
+}
+
+static GOAL: Pos = Pos(4, 6);
+fn main() {
+    let result = bfs(&Pos(1, 1), |p| p.successors(), |p| *p == GOAL);
+    let result = result.expect("no path found");
+    assert_eq!(result.len(), 5);
+    println!("{:?}", result);
+}
