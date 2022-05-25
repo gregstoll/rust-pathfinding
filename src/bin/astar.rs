e2f934cfcf154d31404e62fad6611ae086dac65b
gregstoll <greg@gregstoll.com>
2022-05-25 18:49:45 -0500

    Make bfs only use spaces with cost 1 and more walls, start A* example

--- src/bin/astar.rs
+++ src/bin/astar.rs
@@ -1,6 +1,20 @@
-use pathfinding::prelude::bfs;
+use pathfinding::prelude::astar;
+use rust_pathfinding::{Board, Pos};
 
 //const pattern : 
 fn main() {
-
+    let board = Board::new(vec![
+        "21941",
+        "1X534",
+        "23891",
+        "18215",
+        "13485"], false);
+    let start = Pos(0,1);
+    let goal = Pos(4,2);
+    /*let result = bfs(
+        &start,
+        |p| board.get_successors(p).iter().map(|successor| successor.pos).collect::<Vec<_>>(), 
+        |p| *p==goal);
+    let result = result.expect("No path found");
+    board.draw_to_image(Path::new("board.png"), Some(&result));*/
 }
--- src/bin/bfs.rs
+++ src/bin/bfs.rs
@@ -5,11 +5,11 @@ use rust_pathfinding::{Board, Pos};
 
 fn main() {
     let board = Board::new(vec![
-        "21941",
-        "1X534",
-        "23891",
-        "18215",
-        "13485"], false);
+        "11111",
+        "1X1XX",
+        "1X1X1",
+        "1X111",
+        "11111"], false);
     let start = Pos(0,1);
     let goal = Pos(4,2);
     let result = bfs(
@@ -17,6 +17,5 @@ fn main() {
         |p| board.get_successors(p).iter().map(|successor| successor.pos).collect::<Vec<_>>(), 
         |p| *p==goal);
     let result = result.expect("No path found");
-    //board.draw_to_image(Path::new("board.png"), Some(&vec![Pos(0,0), Pos(0, 1), Pos(1, 2), Pos(2, 2), Pos(2, 1)]));
-    board.draw_to_image(Path::new("board.png"), Some(&result));
+    board.draw_to_image(Path::new("bfs.png"), Some(&result));
 }
