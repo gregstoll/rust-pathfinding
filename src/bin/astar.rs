ed3bcd7194d16368cce8c3708df0519ecc76081d
gregstoll <greg@gregstoll.com>
2022-05-25 20:48:34 -0500

    finish astar

--- src/bin/astar.rs
+++ src/bin/astar.rs
@@ -1,20 +1,24 @@
+use std::path::Path;
+
 use pathfinding::prelude::astar;
 use rust_pathfinding::{Board, Pos};
 
 //const pattern : 
 fn main() {
     let board = Board::new(vec![
-        "21941",
+        "21341",
         "1X534",
-        "23891",
-        "18215",
+        "62891",
+        "16215",
         "13485"], false);
     let start = Pos(0,1);
     let goal = Pos(4,2);
-    /*let result = bfs(
+    let result = astar(
         &start,
-        |p| board.get_successors(p).iter().map(|successor| successor.pos).collect::<Vec<_>>(), 
+        |p| board.get_successors(p).iter().map(|s| (s.pos, s.cost as u32)).collect::<Vec<_>>(),
+        |p| ((p.0 - goal.0).abs() + (p.1 - goal.1).abs()) as u32,
         |p| *p==goal);
     let result = result.expect("No path found");
-    board.draw_to_image(Path::new("board.png"), Some(&result));*/
+    println!("total cost: {:}", result.1);
+    board.draw_to_image(Path::new("astar.png"), Some(&result.0));
 }
