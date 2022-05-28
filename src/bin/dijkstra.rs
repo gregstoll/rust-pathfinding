a9a274f4f2724eb03fc675694e821fe29bfa821d
gregstoll <greg@gregstoll.com>
2022-05-28 14:13:04 -0500

    more interesting dijkstra

--- src/bin/dijkstra.rs
+++ src/bin/dijkstra.rs
@@ -5,16 +5,16 @@ use rust_pathfinding::{Board, Pos};
 
 fn main() {
     let board = Board::new(vec![
-        "21341",
+        "89341",
         "1X534",
         "62891",
-        "16215",
-        "13485"], false);
+        "17214",
+        "13285"], false);
     let start = Pos(0,1);
     let goal = Pos(4,2);
     let result = dijkstra(
         &start,
-        |p| board.get_successors(p).iter().map(|s| (s.pos, s.cost as u32)).collect::<Vec<_>>(),
+        |p| board.get_successors(p).iter().map(|s| (s.pos, s.cost)).collect::<Vec<_>>(),
         |p| *p==goal);
     let result = result.expect("No path found");
     println!("total cost: {:}", result.1);
