135d39f179cf7d16ab3a269189810cc31cdfb5b1
gregstoll <greg@gregstoll.com>
2022-05-28 22:00:23 -0500

    remove unnecessary cast

--- src/bin/astar.rs
+++ src/bin/astar.rs
@@ -16,7 +16,7 @@ fn main() {
     let goal = Pos(6,2);
     let result = astar(
         &start,
-        |p| board.get_successors(p).iter().map(|s| (s.pos, s.cost as u32)).collect::<Vec<_>>(),
+        |p| board.get_successors(p).iter().map(|s| (s.pos, s.cost)).collect::<Vec<_>>(),
         |p| ((p.0 - goal.0).abs() + (p.1 - goal.1).abs()) as u32,
         |p| *p==goal);
     let result = result.expect("No path found");
