1e84b47048387e1468f27e77dbd4db2f161ed413
gregstoll <greg@gregstoll.com>
2022-05-29 20:36:23 -0500

    Make BFS a litlte more interesting (and also stable)

--- src/bin/bfs.rs
+++ src/bin/bfs.rs
@@ -5,13 +5,13 @@ use rust_pathfinding::{Board, Pos};
 
 fn main() {
     let board = Board::new(vec![
-        "11111",
-        "1X1XX",
+        "1111X",
         "1X1X1",
         "1X111",
+        "1X1X1",
         "11111"], false);
     let start = Pos(0,1);
-    let goal = Pos(4,2);
+    let goal = Pos(4,1);
     let result = bfs(
         &start,
         |p| board.get_successors(p).iter().map(|successor| successor.pos).collect::<Vec<_>>(), 
